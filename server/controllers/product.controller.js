9cbb8b812857365991536182b93abea7fc6a2a40
Arkadiusz Zygan <a.zygan@gmail.com>
2022-03-17 13:05:40 +0100

    Connect to mongodb

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,4 +1,7 @@
-export const getAllProducts = function (req, res) {
-  res.send("getAllProducts");
-  res.json(console.log("=> api products"));
+import Product from "../models/product.model.js";
+
+export const getAllProducts = async (req, res) => {
+  try {
+    res.status(200).json(await Product.find());
+  } catch (error) {}
 };
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRoutes from "./routes/product.routes.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 
@@ -10,6 +13,15 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/api", productRoutes);
+
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+let db = mongoose.connection;
+
+db.once("open", () => console.log("Wow!! You're connected to the database!"));
+db.on("error", (error) => console.log("Error " + error));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
