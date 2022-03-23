4e558711dc047ad5d0746faffa6fc8cd043d9d97
Arkadiusz Zygan <a.zygan@gmail.com>
2022-03-23 10:44:28 +0100

    Add scroll to top

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Products from "../../components/Products/ProductsContainer";
 import HomeSlider from "../../components/HomeSlider/HomeSlider";
+import ScrollToTop from "../../components/common/ScrollToTop/ScrollToTop";
 
 const Home = () => {
   return (
     <main className="container">
       <HomeSlider />
       <Products />
+      <ScrollToTop />
     </main>
   );
 };
