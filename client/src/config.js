acf0c8803a27cb745268d8a26fbe5fdd18eb3f5e
Arkadiusz Zygan <a.zygan@gmail.com>
2022-03-24 18:19:10 +0100

    Deploy - netlify

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const API_URI = "http://localhost:5000/api";
+// export const API_URI = "http://localhost:5000/api";
+export const API_URI = "https://bcd-server.herokuapp.com/api";
--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -36,6 +36,7 @@ const Contact = () => {
         </section>
         <section className="contact__right">
           <iframe
+            title="mapBCD"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2483.6457860614946!2d-0.14407868408736707!3d51.501367318965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48760520cd5b5eb5%3A0xa26abf514d902a7!2sBuckingham%20Palace!5e0!3m2!1spl!2spl!4v1648049251116!5m2!1spl!2spl"
             width="100%"
             height="450px"
